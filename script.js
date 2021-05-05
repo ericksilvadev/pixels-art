b3220e02b063bd9010644728a713be731387ee59
Erick Silva <erick.silva6653@gmail.com>
2021-05-05 16:15:53 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// const color = document.getElementsByClassName('color');
-// const palette = document.getElementById('color-palette');
-
-// for (let index = 0; index < color.length; index += 1) {
-//   color[index].addEventListener('click', () => {
-//     if (palette.classList.contains('selected')) {
-//       palette.classList.remove('selected');
-//     } else {
-//       palette.classList.add('selected');
-//       let selected = document.querySelector('.selected');
-//       selected.style.backgroundColor = color[index].style.backgroundColor;
-//       console.log('funcionou');
-//     }
-//   });
-// }
-
+// adicionar o pixel board
 function pixelsColumn(pixelsNumber) {
   const pixelBoard = document.getElementById('pixel-board');
+  // let pixelBoardWidth = pixelsNumber * 42;
   for (let index = 0; index < pixelsNumber; index += 1) {
     const createPixel = document.createElement('div');
     createPixel.classList.add('pixel');
@@ -23,9 +9,30 @@ function pixelsColumn(pixelsNumber) {
   }
 }
 
-// function pixelsLine() {
-//   const pixelLine = pixelsColumn(5);
-
-// }
-
 pixelsColumn(25);
+
+// adidionar a seleção de cores
+const color = document.getElementsByClassName('color');
+const palette = document.getElementById('color-palette');
+
+palette.addEventListener('click', () => {
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].classList.remove('selected');
+    event.target.classList.add('selected');
+    const selected = document.querySelector('.selected');
+    selected.style.backgroundColor = color[index].style.backgroundColor;
+  }
+});
+
+// for (let index = 0; index < color.length; index += 1) {
+//   color[index].addEventListener('click', () => {
+//     if (color[index].classList.contains('selected')) {
+//       color[index].classList.remove('selected');
+//     } else {
+//       color[index].classList.add('selected');
+//       const selected = document.querySelector('.selected');
+//       selected.style.backgroundColor = color[index].style.backgroundColor;
+//       console.log('funcionou');
+//     }
+//   });
+// }
