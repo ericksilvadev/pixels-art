902dd19f7e9db1f53a3ac62c00c28c1458a3ee97
Erick Silva <erick.silva6653@gmail.com>
2021-05-05 18:38:53 -0300

    requisito 7, 9 e quase 8

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function pixelsColumn(pixelsNumber) {
 pixelsColumn(25);
 
 // adidionar a seleção de cores
-const color = document.getElementsByClassName('color');
 const palette = document.getElementById('color-palette');
+const color = document.getElementsByClassName('color');
 
 palette.addEventListener('click', (event) => {
   for (let index = 0; index < color.length; index += 1) {
@@ -35,17 +35,20 @@ palette.addEventListener('click', (event) => {
 //   });
 // }
 
-// colorir pixels
-
 // for (let index = 0; index < color)
 
 // let colorSelected = document.querySelector('.selected');
 // const selected = document.querySelector('.selected');
 // selected.style.backgroundColor = color[0];
+
+// colorir pixels
+
+const selected = document.querySelector('.selected');
+const colorSelected = selected.style.backgroundColor;
+console.log(colorSelected);
 const pixels = document.getElementsByClassName('pixel');
 
 for (let index = 0; index < pixels.length; index += 1) {
-  let colorSelected = color[0].style.backgroundColor;
   pixels[index].addEventListener('click', () => {
     pixels[index].style.backgroundColor = colorSelected;
   });
